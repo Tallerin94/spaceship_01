09fec67c8bc0cb4efe401b8aee6183dd66757ca4
ifrokz <dyfrok@gmail.com>
2016-10-27 19:00:37 +0200

    Estoy probando a crear una nave dibujada con objetos y fillStyle (se acaba la clase por eos lo dejo asi)

---
+++ cdn/js/init.js
@@ -0,0 +1,4 @@
+var loop_string = "";
+
+var lienzo = document.getElementById("lienzo");
+var ctx = lienzo.getContext("2d"); 
--- cdn/js/main.js
+++ cdn/js/main.js
@@ -1,16 +1,40 @@
-var temporizador = "";
-
 $(document).ready(function(){
     start();
 });
 
+function Nave(type,life,ammo,speed,posx,posy){
+    this.type = type;
+    this.life = life;
+    this.ammo = ammo;
+    this.speed = speed;
+    this.posx = posx;
+    this.posy = posy;
+}
+
+Nave.prototype.playerMove = function(){
+    this.posx+=this.speed;
+}
+var player = new Nave(1,3,10,5,100,window.innerHeight/2);
+
 function start(){
     $("#lienzo").attr("height", window.innerHeight);
     $("#lienzo").attr("width", window.innerWidth);
-    temporizador = setTimeout("loop()", 1000);
+    loop_string = setTimeout("main_menu()", 10);
+}
+
+function main_menu(){
+    $(document).keydown(function(){
+        $("#main_menu").css("visibility","hidden");
+         loop_string = setTimeout("loop()", 1000);
+    });
 }
 
 function loop(){
-    clearTimeout(temporizador);
-    setTimeout("loop()",33);
+    ctx.clearRect(0,0,window.innerWidth,window.innerHeight);
+    ctx.fillStyle = "red";
+    player.playerMove();
+    ctx.fillRect(player.posx,player.posy,20,20);
+    //console.log("xd");
+    clearTimeout(loop_string);
+    loop_string = setTimeout("loop()",33);
 }
