b64cffb604cb5f0ef8bc4c98bc2ebf4baf3c36ec
Tallerin94 <taler94bcn@gmail.com>
2016-10-29 17:28:29 +0200

    enemy1 move

--- cdn/js/class/cEnemy1.js
+++ cdn/js/class/cEnemy1.js
@@ -1,8 +1,24 @@
 var Enemy1 = function(){
-    this.enemyMove = function(){
-
+    var dirEnemy1 = "top";
+    this.enemyMoveX = function(){
+        enemy1.newSpeedX(enemy1.getSpeedX());
+    }
+    this.enemyMoveY = function(){
+        if(dirEnemy1 == "top"){
+            enemy1.newSpeedY(enemy1.getSpeedY());
+            if(enemy1.posY <= 0){
+                dirEnemy = "bottom";
+                enemy1.setSpeedY(5)
+            }
+        }else{
+            enemy1.newSpeedY(enemy1.getSpeedY());
+            if(enemy1.posY >= window.innerHeight){
+                dirEnemy = "top";
+                enemy1.setSpeedY(-5);
+            }
+        }
     }
 }
-//Enemy.prototype = new Ship(101, 100, 0, 0, window.innerWidth-100, window.innerHeight/2);
+Enemy1.prototype = new Ship(101, 100, -5, -5, window.innerWidth-100, window.innerHeight/2);
 
-//var enemy1 = new Enemy1();
+var enemy1 = new Enemy1();
