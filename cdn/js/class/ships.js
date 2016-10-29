e03cb4cb98541cf7c9501ce18038ff84c6863fb3
Tallerin94 <taler94bcn@gmail.com>
2016-10-29 14:03:23 +0200

    Limitar velocidades

--- cdn/js/class/ships.js
+++ cdn/js/class/ships.js
@@ -79,22 +79,36 @@ Ship = function(type,life,ammo,speedX,speedY,posX,posY){
 Player = function(){
     this.playerMove = function(){
         // LEFT / RIGHT
-        if(dirPosX=="left"){player.newSpeedX(player.getSpeedX()-1);}
-        if(dirPosX=="right"){player.newSpeedX(player.getSpeedX()+1);}
-        if(player.getSpeedX()>14){player.newSpeedX(14);}
-        if(player.getSpeedX()<-7){player.newSpeedX(-7);}
+        if(dirPosX == "left"){player.newSpeedX(player.getSpeedX()-1);}
+        if(dirPosX == "right"){player.newSpeedX(player.getSpeedX()+1);}
+        if(player.getSpeedX()>14){
+            // Limitador de velocidad en diagonal
+            if(dirPosY == "up" || dirPosY == "down"){
+                player.newSpeedX(10);
+            }else{
+                player.newSpeedX(14);
+            }
+        }
+        if(player.getSpeedX()<-7){
+            // Limitador de velocidad en diagonal
+            if(dirPosY == "up" || dirPosY == "down"){
+                player.newSpeedX(-7);
+            }else{
+                player.newSpeedX(-4);
+            }
+        }
         player.setPosX();
         // UP / DOWN
-        if(dirPosY=="up"){player.newSpeedY(player.getSpeedY()-1);}
-        if(dirPosY=="down"){player.newSpeedY(player.getSpeedY()+1);}
+        if(dirPosY == "up"){player.newSpeedY(player.getSpeedY()-1);}
+        if(dirPosY == "down"){player.newSpeedY(player.getSpeedY()+1);}
         if(player.getSpeedY()>10){player.newSpeedY(10);}
         if(player.getSpeedY()<-10){player.newSpeedY(-10);}
         player.setPosY();
         // NONE 
-        if(dirPosY=="" && player.getSpeedY()<0){player.newSpeedY(player.getSpeedY()+0.5);}
-        if(dirPosY=="" && player.getSpeedY()>0){player.newSpeedY(player.getSpeedY()-0.5);}
-        if(dirPosX=="" && player.getSpeedX()<0){player.newSpeedX(player.getSpeedX()+0.5);}
-        if(dirPosX=="" && player.getSpeedX()>0){player.newSpeedX(player.getSpeedX()-0.5);}
+        if(dirPosY == "" && player.getSpeedY()<0){player.newSpeedY(player.getSpeedY()+0.5);}
+        if(dirPosY == "" && player.getSpeedY()>0){player.newSpeedY(player.getSpeedY()-0.5);}
+        if(dirPosX == "" && player.getSpeedX()<0){player.newSpeedX(player.getSpeedX()+0.5);}
+        if(dirPosX == "" && player.getSpeedX()>0){player.newSpeedX(player.getSpeedX()-0.5);}
         //  
     }
 }
